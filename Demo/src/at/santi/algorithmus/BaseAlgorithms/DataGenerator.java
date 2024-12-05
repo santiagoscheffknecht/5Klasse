56acfb441be9bcad87b37608f5c9d06ade9b1013
santiago <santiago.scheffknecht@student.htldornbirn.at>
2024-12-05 10:38:28 +0100

    Generatur in range finished

--- Demo/src/at/santi/algorithmus/BaseAlgorithms/DataGenerator.java
+++ Demo/src/at/santi/algorithmus/BaseAlgorithms/DataGenerator.java
@@ -4,14 +4,31 @@ import java.util.Random;
 
 public class DataGenerator {
 
-    // Methode: Generiert ein Array mit size zufälligen Werten aus dem
-    // Integer-Bereich
+    // Generiert ein Array mit zufälligen Werten
     public static int[] generateDataArray(int size) {
         Random random = new Random();
         int[] dataArray = new int[size];
         for (int i = 0; i < size; i++) {
-            dataArray[i] = random.nextInt(); // Ganzzahliger Bereich von Integer
+            dataArray[i] = random.nextInt();
         }
         return dataArray;
+    }
+
+    // Generiert ein Array mit zufälligen Werten innerhalb eines Bereich
+    public static int[] generateDataArray(int size, int min, int max) {
+        Random random = new Random();
+        int[] dataArray = new int[size];
+        for (int i = 0; i < size; i++) {
+            dataArray[i] = random.nextInt((max - min) + 1) + min;
+        }
+        return dataArray;
+    }
+
+    // Gibt die Werte des Arrays in einer Zeile auf der Konsole aus
+    public static void printArray(int[] data) {
+        for (int value : data) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
     }
 }
